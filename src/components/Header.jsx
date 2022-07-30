416deb0a078021b6dcd3c265d00aaa3bb71ea246
AllKoff <axkoff@gmail.com>
2022-07-30 18:41:46 +0300

    lesson 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
         <img src='https://c-hub.kh.ua/wp-content/uploads/2021/12/18122021-390x205.jpg' />
     </header>;
 }
