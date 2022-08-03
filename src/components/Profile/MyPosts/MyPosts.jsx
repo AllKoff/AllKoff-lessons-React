cbab6bcb10b5cbfd4b8f448c14a5ae51e1d04aa0
AllKoff <axkoff@gmail.com>
2022-08-03 14:43:23 +0300

    lesson 19.1

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,24 +3,22 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-  return (
-    <div>
-      my post
-      <div>
-        <textarea></textarea>
-        <button>Add post</button>
-        <button>Remove</button>
-      </div>
-      <div className={classes.post}>
-        <Post message= 'Hi, how are yoy' Post Like= ' 15 ' Post DisLike= ' 125 ' />
-      
-              </div>
-
-      <div className={classes.post}>
-        <Post message= 'It`s my first post' Post Like= ' 20 ' Post DisLike= ' 3 ' />
-      </div>   
-  </div>
-  )
-  }
+    return (
+        <div>
+            my post
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+                <button>Remove</button>
+            </div>
+            <div className={classes.рost}>
+                <Post message='Hi, how are yoy' Post Like=' 15 ' Post DisLike=' 125 '/>
+            </div>
+            <div className={classes.post}>
+                <Post message='It`s my first post' Post Like=' 20 ' Post DisLike=' 3 '/>
+            </div>
+        </div>
+    )
+}
 
 export default MyPosts;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import classes from './Post.module.css';
 
 const Post = (props) => {
-  return (
-          <div className={classes.item}>
-        <img src='https://images-na.ssl-images-amazon.com/images/S/pv-target-images/8bbae0ddacf7ead8ee07b66e6f5feb8ba15d0307d955a50f7dab7fb50fd9b21d._RI_V_TTW_.jpg' />
-        
-        { props.message } 
-        <div>
-            <span>Like { props.Like }</span>
-          </div>
-          <div>
-            <span>DisLike { props.DisLike }</span>
-          </div>
-          
-          
-      </div>
-  )
+    return (
+        <div className={classes.item}>
+            <img
+                src='https://images-na.ssl-images-amazon.com/images/S/pv-target-images/8bbae0ddacf7ead8ee07b66e6f5feb8ba15d0307d955a50f7dab7fb50fd9b21d._RI_V_TTW_.jpg'/>
+
+            {props.message}
+            <div>
+                <span>Like {props.Like}</span>
+            </div>
+            <div>
+                <span>DisLike {props.DisLike}</span>
+            </div>
+        </div>
+    )
 }
 
 export default Post;
