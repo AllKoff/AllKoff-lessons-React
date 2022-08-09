88883d599d8a59c373165aaba7b7bfdab3d9a483
AllKoff <axkoff@gmail.com>
2022-08-09 15:32:17 +0300

    lesson 24 correct

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -11,7 +11,7 @@ const DialogItem = (props) => {
         Все оборачиваем в фигурные скобки */}
         {/*<NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>  {'/dialogs/' + props.id} - выносим в
         отдельную переменную path */}
-        <NavLink to={path}>{props.name}</NavLink>     {/* все обернуто в фигурные скобки это js-код   */}
+        <NavLink to={path}>{props.name}</NavLink> {/* все обернуто в фигурные скобки это js-код   */}
     </div>
 }
 
@@ -20,26 +20,43 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Alex'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Svetlana'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How are you, Alex?'},
+        {id: 3, message: 'Yo!'},
+        {id: 4, message: 'Yo!'},
+        {id: 5, message: 'Yo!'}
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
 
-                <DialogItem name="Alex" id="1"/>
-                <DialogItem name="Andrey" id="2"/>
-                <DialogItem name="Svetlana" id="3"/>
-                <DialogItem name="Sasha" id="4"/>
-                <DialogItem name="Viktor" id="5"/>
-                <DialogItem name="Valera" id="6"/>
+                <DialogItem name={dialogsData[0] .name} id={dialogsData[0] .id}/>
+                <DialogItem name={dialogsData[1] .name} id={dialogsData[1] .id}/>
+                <DialogItem name={dialogsData[2] .name} id={dialogsData[2] .id}/>
+                <DialogItem name={dialogsData[3] .name} id={dialogsData[3] .id}/>
+                <DialogItem name={dialogsData[4] .name} id={dialogsData[4] .id}/>
+                <DialogItem name={dialogsData[5] .name} id={dialogsData[5] .id}/>
 
 
             </div>
             <div className={classes.messages}>
-                <Message message='Hi!'/>  {/*  в теге Message организовали атрибут message для предачи его
-                в props компонента Message*/}
-                <Message message='How are you, Alex?'/>
-                <Message message='Yo!'/>
-                <Message message='Yo!'/>
-                <Message message='Yo!'/>
+                <Message message={messagesData[0] .message}/>
+                <Message message={messagesData[1] .message}/>
+                <Message message={messagesData[2] .message}/>
+                <Message message={messagesData[3] .message}/>
+                <Message message={messagesData[4] .message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'Hi, how are yoy', Like: 15, DisLike: 125},
+        {id: 2, message: 'It`s my first post', Like: 20, DisLike: 3}
+    ]
+    // Здесь мы сформировали массив с данными
+
     return (
         <div className={classes.postsBlok}>
             <h3> My post </h3>
@@ -14,9 +21,11 @@ const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={classes.posts}>
-                <Post message='Hi, how are yoy' Like=' 15 ' DisLike=' 125 '/>
-                <Post message='It`s my first post' Like=' 20 ' DisLike=' 3 '/>
+                <Post message={postData[0].message} Like={postData[0].Like} DisLike={postData[0].DisLike}/>
+                <Post message={postData[1].message} Like={postData[1].Like} DisLike={postData[1].DisLike}/>
+                {/* Здесь мы подготовили код html для отрисовки с данными из массива*/}
             </div>
+
         </div>
     )
 }
