36d02aca135c7be4e538a4c94c0ef427bcb888de
AllKoff <axkoff@gmail.com>
2022-08-10 19:18:16 +0300

    lesson 27 correct

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import Massage from "./components/Dialogs/Massage/Massage";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -22,9 +21,9 @@ const App = (props) => {
 
                         <Route path='/Profile' element={<Profile/>}/>
                         <Route path='/Dialogs/*' element={<Dialogs/>}/>
-                        <Route path='/News' element={<News/>}/>
-                        <Route path='/Music' element={<Music/>}/>
-                        <Route path='/Settings' element={<Settings/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
 
                     </Routes>
 
